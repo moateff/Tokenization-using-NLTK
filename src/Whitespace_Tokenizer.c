c840b029b4d8078ac45e52c35af8d4f78ddd71b4
moateff <moatef.yousef@gmail.com>
2024-10-06 01:59:00 +0300

    Commit Whitespace_Tokenizer.c

--- src/Whitespace_Tokenizer.c
+++ src/Whitespace_Tokenizer.c
@@ -5,8 +5,21 @@
 #define MAX_LENGTH 50
 #define MAX_TOKENS 10
 
-char input[MAX_LENGTH];
-char *tokens[MAX_TOKENS];
+int get_line(char *ptr){
+    int ch, n = 0;
+    while ((ch = getchar()) != '\n'){
+        *(ptr++) = (char)ch;
+        n++;
+    }
+    *ptr = '\0';
+    return n;
+}
+
+int read_input(char *input){
+    int count = 0;
+    printf("\n> ");
+    return count = get_line(input);
+}
 
 int parse(char *input, char **tokens){
     int count = 0;
@@ -20,3 +33,25 @@ int parse(char *input, char **tokens){
     *tokens = '\0';
     return  count;
 }
+
+void print_tokens(char **tokens, int n){
+    for(int i = 0; i < n; i++) printf("%s\n", tokens[i]);
+}
+
+int main(){
+    char input[MAX_LENGTH];
+    char *tokens[MAX_TOKENS];
+    int num_of_tokens;
+    while(true){
+        read_input(input);
+        num_of_tokens = parse(input, tokens);
+        print_tokens(tokens, num_of_tokens);
+    }
+    return 0;
+}
+
+// > Machine learning is fascinating   
+// Machine
+// learning
+// is
+// fascinating
